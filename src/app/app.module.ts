49e409bded16cee957536769c4d1a1eb1fabbb6f
ottoauravuo <42932135+ottoauravuo@users.noreply.github.com>
2021-03-18 20:16:41 +0200

    Pohja tehty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageComponent
+    PageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
