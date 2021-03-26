0931a87d7cae29c3ef9079a88c4aa2a126e79dc2
ottoauravuo <42932135+ottoauravuo@users.noreply.github.com>
2021-03-26 17:23:55 +0200

    .

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -2,9 +2,9 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("header").style.fontSize = "30px";
+    document.getElementById("header").style.fontSize = "15px";
   } else {
-    document.getElementById("header").style.fontSize = "90px";
+    document.getElementById("header").style.fontSize = "20px";
   }
 }
 
