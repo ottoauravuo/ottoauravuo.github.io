cfdc4098252cbabf8b74e502f240de0e0de63b0a
Ketomaa <tketomaa5@gmail.com>
2021-04-04 16:38:52 +0300

    Update javascript.js

--- src/assets/js/javascript.js
+++ src/assets/js/javascript.js
@@ -10,41 +10,28 @@ function changeBodyLanguageENG(){
   document.getElementById("whatsApp").innerHTML = "Looking for people to play with? Join our WhatsApp group!";
   document.getElementById("whatsAppLink").innerHTML = "WHATSAPPLINK PLACEHOLDER";
   document.getElementById("timmi").innerHTML = "TIMMI PLACEHOLDER";
-  document.getElementById("padelRulesTitle").innerHTML = "Rules of the game:";
-  document.getElementById("padelRules").innerHTML = "Matches \n" +
-    "\n" +
-    "are always played in doubles and the best of the three sets wins. \n" +
-    "\n" +
-    "The first to win 6 games wins the round, after they have a lead with a difference of two rounds.  \n" +
-    "\n" +
-    "At the beginning of the game, the teams decide which side they'll play.  \n" +
-    "\n" +
-    "The orientation lasts until the half-game, after which the sides can be changed. \n" +
-    "Scoring \n" +
-    "\n" +
-    "is the same as in tennis (15, 30, 40 and the game; in a draw 40-40, two consecutive points are needed for a team to win the game). \n" +
-    "\n" +
-    "When the game is tied 6-6, the winners are decided by playing a tiebreaker, which is played until 7 points, however again, with a lead of two points. \n" +
-    "The start of the game is made with a tap-off. \n" +
-    "The serve is decided by the winner of the tap-off. \n" +
-    "In the tap-off, all players are going to be hitting the ball once onto the opponent's field, " +
-    "after which the winners of the tap-off can decide which side they'll play and will perform the serve. \n" +
-    "The serve must bounce off the opponent's square. \n" +
-    "After the bounce, the ball can hit the glass wall, but not the net wall. \n" +
-    "If the serve hits the net but is otherwise correct, the player can have a second attempt. \n" +
-    "During the serve, the teammate must be close to the net. \n" +
-    "The first point is served from the right-side square, after that from the left and then right back-and-forth again.  \n" +
-    "After the end of a match, the serve passes to the opponent. \n" +
-    "After the serve, all of the ground becomes the playing field. \n" +
-    "The serve cannot be hit back instantly from the air, but only after the return of the serve, the ball may be hit directly from the air, " +
-    "after a bounce, either after a bounce from your own glass wall and directly from the air after it bounced off the opponent’s glass wall. " +
-    "The ball cannot be hit on the opponent's side through the net wall. \n" +
-    "\n" +
-    "During a ball rally, the ball may not touch the ground on the same side of the field more than once. \n" +
-    "The game is played \n" +
-    "\n" +
-    "until the ball gets stuck in the net, the ball falls onto your own side after hitting the net, " +
-    "the ball bounces twice off the ground on the same side of the field, or the ball hits the glass wall or net on the opponent's side before first touching the ground. ";
+  document.getElementById("padelRulesTitle").innerHTML = "<strong>Rules of the game:</strong>";
+  document.getElementById("padelRules").innerHTML = "<strong>Matches</strong> are always played in doubles and the best of the three sets wins. The first to win six games wins the round, after they have a lead with a difference of two rounds. At the beginning of the game, the teams decide which side they'll play. The orientation lasts until the half-game, after which the sides can be changed.\n" +
+  "\n" +
+  "      <br><br><strong>Scoring</strong> is the same as in tennis (15, 30, 40 and the game; in a draw 40-40, two consecutive points are needed for a team to win the game). When the game is tied 6-6, the winners are decided by playing a tiebreaker, which is played until seven points, however again, with a lead of two points.\n" +
+  "\n" +
+  "      <br><br><strong>The start of the game</strong> is made with a tap-off. The serve is decided by the winner of the tap-off. In the tap-off, all players are going to be hitting the ball once onto the opponent's field, after which the winners of the tap-off can decide which side they'll play and will perform the serve.\n" +
+  "\n" +
+  "      <br><br><strong>The Serve</strong> must bounce the ball on the ground within the service box in which they find themselves to carry out the serve. In the first instance the serve must go into the receiver’s box located at his left side. The height of the ball being served must be at or below the waist level at the moment of hitting it, and the player must have at least one foot in contact with the ground. The person serving has two attempts at making a successful serve.\n" +
+  "\n" +
+  "      The serve must bounce off the opponent's square. After the bounce, the ball can hit the glass wall, but not the net wall.If the serve hits the net but is otherwise correct, the player can have a second attempt. During the serve, the teammate must be close to the net.\n" +
+  "\n" +
+  "      The first point is served from the right-side square, after that from the left and then right back-and-forth again. After the end of a match, the serve passes to the opponent.\n" +
+  "\n" +
+  "      After the serve, all of the ground becomes the playing field.\n" +
+  "\n" +
+  "      The serve cannot be hit back instantly from the air, but only after the return of the serve, the ball may be hit directly from the air, after a bounce, either after a bounce from your own glass wall and directly from the air after it bounced off the opponent’s glass wall. The ball cannot be hit on the opponent's side through the net wall. During a ball rally, the ball may not touch the ground on the same side of the field more than once.\n" +
+  "\n" +
+  "      <br><br><strong>The game is played</strong> until the ball gets stuck in the net, the ball falls onto your own side after hitting the net, the ball bounces twice off the ground on the same side of the field, or the ball hits the glass wall or net on the opponent's side before first touching the ground.\n" +
+  "\n" +
+  "      <br><br><strong>Tie-break</strong> happens when the game is in a 6-6 situation. The serve in tie-break is made from the right-side square by the next player in the serve, who will hit for one point. After this, the serve is passed onto the opponent, who will hit for two points, from the opposite side's left square. The tie-break will go on until seven points are reached, with a two-point lead over the opponent. The serve changes every two points to the opponent and the first serve is always played from the second square.\n" +
+  "\n" +
+  "      <br><br><strong>Hitting the ball off the field.</strong> If a player hits the ball onto the opponent's field and it bounces off the court, the batsman will usually win the point. However, in some cases, you can go and run out of the doorways to try and hit the ball back into the court and have it land on the opponent’s field.</p>";
   document.getElementById("parkingInstructions").innerHTML = "PARKING PLACEHOLDER";
   document.getElementById("contactInfoTitle").innerHTML = "Contact Info";
 }
@@ -62,7 +49,7 @@ function changeBodyLanguageFIN(){
   document.getElementById("whatsApp").innerHTML = "Liity Padel WhatsApp-ryhmään ja löydä pelikavereita!";
   document.getElementById("whatsAppLink").innerHTML = "Linkki WhatsApp- ryhmään: toBEMaDe@WhatsAPP";
   document.getElementById("timmi").innerHTML = "Timmin käyttöohjeet (tulossa)";
-  document.getElementById("padelRulesTitle").innerHTML = "Padelin säännöt:";
+  document.getElementById("padelRulesTitle").innerHTML = "<strong>Padelin säännöt:</strong>";
   document.getElementById("padelRules").innerHTML = "<strong>Ottelut</strong> pelataan aina nelinpelinä ja paras kolmesta erästä voittaa. Ensimmäinen kuusi peliä voittanut voittaa erän, kun heillä on kahden erän erotus. Pelaajapari päättää erän alussa kumpaa puolta kumpikin pelaa. Puoli säilyy erän loppuun asti, jonka jälkeen puolta voi vaihtaa.\n" +
     "\n" +
     "      <br><br><strong>Pistelasku</strong> sama kuin tenniksessä (15, 30, 40 ja peli; tasatilanteessa 40-40 tarvitaan kaksi peräkkäistä pistettä pelin voittamiseksi). Pelien ollessa tasan 6-6, erävoitto ratkaistaan pelaamalla tie-break, joka pelataan 7 pisteeseen, kuitenkin kahden pisteen erolla.\n" +
